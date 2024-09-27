f5c63ceb24a64fe06e2b6a23139842c48a314c7a
Diego Alonso Cárcamo Gutiérrez <diegocarcamo.contacto@gmail.com>
2024-09-27 12:57:48 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,8 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        int num=0;
+        doWhileLoop(num);
     }
 
     //TODO ↓
@@ -12,6 +13,10 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
-
+        int x=0;
+        do {
+            System.out.println("Entra con valor: "+x+", sale con valor: "+(x+1));
+            x=x+1;
+        }while (x<num);
     }
 }
